b7fd7e3fa8695f770d04f5a6e4e97e500d671e67
m-nikolov95 <m-nikolov95@users.noreply.github.com>
2026-05-07 03:10:38 +0300

    Removed unneeded empty line

--- src/components/tasks-component/TasksComponent.tsx
+++ src/components/tasks-component/TasksComponent.tsx
@@ -54,7 +54,6 @@ export function TasksComponent(): JSX.Element {
                         Add Task
                     </button>
                 </div>
-
             </div>
         </div>
     )
